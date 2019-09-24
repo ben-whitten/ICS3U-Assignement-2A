bb22da93742ef95ddd5a3124c6f559aa4cf8b2a7
ben-whitten <benjamin.whitten@mths.ca>
2019-09-24 00:30:47 +0000

    added program

---
+++ icosahedron.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Created by: Ben Whitten
+# Created on: September 2019
+# This is a program which can find the volume of an icosahedron.
+
+
+import math
+
+
+def main():
+
+    # Input
+    length = int(input("input the edge length of the icosahedron:"))
+
+    # Process
+    volume = 5*(3+math.sqrt(5))/12*length**3
+
+    # Output
+    print("")
+    print("Then the volume of the icosahedron is {}cm^2" .format(volume))
+
+
+if __name__ == "__main__":
+    main()
